dc3f780458c74f1403baffd98c47bbfcd90bb09b
Federico <federico.diaferia@gmail.com>
2025-03-31 17:38:59 +0200

    adding games

--- Us_States_Game/main.py
+++ Us_States_Game/main.py
@@ -10,8 +10,8 @@ turtle.addshape(image)
 turtle.shape(image)
 
 data = pd.read_csv("50_states.csv")
-total_states = len(data)
-correct_states = 0
+list_states = data.state.tolist()
+correct_states = []
 
 def writing_state(x, y, state):
     t = turtle.Turtle()
@@ -21,24 +21,28 @@ def writing_state(x, y, state):
     t.pendown()
     t.write(f"{state}", False, align="center", font=("Arial", 10, "normal"))
 
-def game_over():
-    t = turtle.Turtle()
-    t.hideturtle()
-    t.penup()
-    t.pendown()
-    turtle.write(f"Congrats!! you have guessed all the {total_states} States", False, align="center",font=("Arial", 18, "normal"))
+while len(correct_states) < 50:
+    answer_state = screen.textinput(title=f"{len(correct_states)}/50 correct States",
+                                    prompt="What's another state's name?").title()
 
-
-while correct_states < total_states:
-    answer_state = screen.textinput(title=f"{correct_states}/{total_states} correct States", prompt="What's another State's name").title()
-    check_state = data[data["state"] == answer_state]
-    if check_state.empty or answer_state is None:
+    if answer_state is None:
         continue
-    elif check_state.state.iloc[0] == answer_state:
-        correct_states += 1
-    x_coordinate = float(check_state.x.iloc[0])
-    y_coordinate = float(check_state.y.iloc[0])
-    writing_state(x_coordinate, y_coordinate, answer_state)
-game_over()
+
+    check_state = data[data["state"] == answer_state]
+
+    if not check_state.empty and answer_state not in correct_states:
+        correct_states.append(answer_state)
+        x_coordinate = float(check_state.x.iloc[0])
+        y_coordinate = float(check_state.y.iloc[0])
+        writing_state(x_coordinate, y_coordinate, answer_state)
+
+    if answer_state == "Exit":
+        missing_states = []
+        for state in list_states:
+            if state not in correct_states:
+                missing_states.append(state)
+        new_data = pd.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
 
 screen.mainloop()
