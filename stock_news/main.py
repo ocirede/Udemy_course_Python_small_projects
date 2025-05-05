3927847adeb842d067479eb3d3644d36f9ba23ef
Federico Diaferia <federico.diaferia@gmail.com>
2025-05-05 19:23:32 +0200

    adding

--- stock_news/main.py
+++ stock_news/main.py
@@ -40,41 +40,38 @@ stock_params = {
     "apikey": api_stock
 }
 
-
 response_stock = requests.get(STOCK_ENDPOINT, params=stock_params)
 response_stock.raise_for_status()
 stock_data = response_stock.json()
-price_data  = stock_data["Time Series (Digital Currency Daily)"]
+price_data = stock_data["Time Series (Digital Currency Daily)"]
 today_close = float(price_data[str(today)]["4. close"])
 yesterday_close = float(price_data[str(yesterday)]["4. close"])
 
-percentage_change = ((today_close - yesterday_close) / yesterday_close) * 100
+
+def main():
+    percentage_change = ((today_close - yesterday_close) / yesterday_close) * 100
+    if percentage_change > 5:
+        response_news = requests.get(NEWS_ENDPOINT, params=parameters)
+        response_news.raise_for_status()
+        data = response_news.json()
+        articles = data["articles"][0:3]
+        random_article = random.choice(articles)
+        symbol = "🔺" if today_close > yesterday_close else "🔻"
+        message_body = (
+            f"{STOCK}: {symbol}{percentage_change:.2f}%,\n\n"
+            f"Headline: {random_article['title']}\n\n"
+            f"Brief: {random_article['description']}")
+        client = Client(account_sid, auth_token)
+        try:
+            message = client.messages.create(
+                from_=virtual_phone,
+                body=message_body,
+                to=phone_number
+            )
+            print("Message SID:", message.sid)
+        except Exception as e:
+            print("Failed to send message:", e)
 
 
-if percentage_change > 5:
-    response_news = requests.get(NEWS_ENDPOINT, params=parameters)
-    response_news.raise_for_status()
-    data = response_news.json()
-    articles = data["articles"][0:3]
-    random_article = random.choice(articles)
-    symbol = "🔺" if today_close > yesterday_close else "🔻"
-    message_body = (
-        f"{STOCK}: {symbol}{percentage_change:.2f}%,\n\n"
-        f"Headline: {random_article['title']}\n\n"
-        f"Brief: {random_article['description']}")
-    client = Client(account_sid, auth_token)
-    try:
-        message = client.messages.create(
-            from_=virtual_phone,
-            body= message_body,
-            to= phone_number
-        )
-        print("Message SID:", message.sid)
-    except Exception as e:
-        print("Failed to send message:", e)
-
-
-
-
-
-
+if __name__ == "__main__":
+    main()
